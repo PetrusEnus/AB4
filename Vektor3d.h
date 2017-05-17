1aa8900c73c988513e4cef8e9b2f30615156a589
PetrusEnus <zehrsa78@gmail.com>
2017-05-17 20:15:53 +0200

    Update Vektor3d.h

--- Vektor3d.h
+++ Vektor3d.h
@@ -3,9 +3,15 @@
 
 class Vektor3d{
     float x, y, z;
+    unsigned int i, j; 
 public:
     Vektor3d(float a, float b, float c);
+    Vektor3d();
     ~Vektor3d();
+    
+    void get();
+    
+    friend bool operator + (const Vektor3d& op)const;
 };
 
 #endif /* VEKTOR3D_H */
