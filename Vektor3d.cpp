70e8774a5cba923b1f4edf844c05c2e9949a32e9
PetrusEnus <zehrsa78@gmail.com>
2017-05-17 20:15:25 +0200

    Update Vektor3d.cpp

--- Vektor3d.cpp
+++ Vektor3d.cpp
@@ -4,15 +4,28 @@
 
 using namespace std;
 
-Vektor3d::Vektor3d(float a, float b, float c){
-    x = 0;
-    y = 0;
-    z = 0;
-    x = a;
-    y = b;
-    z = c;
+i = 0;
+j = 0;
+
+Vektor3d::Vektor3d(float a, float b, float c):x(a),y(b),z(c){
+
+}
+
+Vektor3d::Vektor3d(){
+    x=0;
+    y= 0;
+    z=0;
 }
 
 Vektor3d::~Vektor3d() {
 }
 
+friend bool Vektor3d::operator + (const Vektor3d& op)const{
+    Vektor3d tmp(*this);
+    tmp.i += op.i;
+    
+}
+
+void Vektor3d::get(){
+    cout << "x: " << x << "| y: " << y << "| z: " << z;
+}
