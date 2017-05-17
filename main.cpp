9e7b06327625d2b3a15224063b6886b15df0c1c9
PetrusEnus <zehrsa78@gmail.com>
2017-05-17 20:16:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <cstdlib>
 #include <iostream>
+#include "Vektor3d.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
+    //Vektor3d vac();
+    Vektor3d vacP(3.34 , 4.12 , 5.24);
     
+    //vac.get();
+    vacP.get();
+    
+    
+    //cout << vac << "\n";
     return 0;
 }
 
